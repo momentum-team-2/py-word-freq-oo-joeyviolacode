bf3983779ca2d038035b6f5d8b58b1a1bb515edd
joeyviolacode <joeyviolacode@gmail.com>
2020-06-22 12:49:11 -0400

    OO implemented

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import re
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
     'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
@@ -7,19 +9,19 @@ STOP_WORDS = [
 
 class FileReader:
     def __init__(self, filename):
-        pass
+        self.filename = filename
 
     def read_contents(self):
-        """
-        This should read all the contents of the file
-        and return them as one string.
-        """
-        raise NotImplementedError("FileReader.read_contents")
+        this_file = open(self.filename)
+        text = this_file.read()
+        this_file.close()
+        return text
 
 
 class WordList:
     def __init__(self, text):
-        pass
+        self.text = text
+        self.word_list = []
 
     def extract_words(self):
         """
@@ -27,14 +29,18 @@ class WordList:
         is responsible for lowercasing all words and stripping
         them of punctuation.
         """
-        raise NotImplementedError("WordList.extract_words")
+        str = self.text.lower()
+        words = re.sub(r'[?|—|:|"|,|\.\n|\.|\s|\n|\t|\v|\f|\r]+', "*", str)
+        self.word_list = words.split("*")
+        
 
     def remove_stop_words(self):
         """
         Removes all stop words from our word list. Expected to
         be run after extract_words.
         """
-        raise NotImplementedError("WordList.remove_stop_words")
+        self.word_list = [word for word in self.word_list if len(word) > 1 and word not in STOP_WORDS] #The len(word) check is here because there's still one piece of white space I haven't pinned down in each file.  I haven't taken the time to figure out a quick way to look at all the whitespace characters yet, but none of the ones I included takes care of that one lonely space.  Will keep on it.
+        self.word_list.sort()
 
     def get_freqs(self):
         """
@@ -43,12 +49,20 @@ class WordList:
         extract_words and remove_stop_words. The data structure
         could be a dictionary or another type of object.
         """
-        raise NotImplementedError("WordList.get_freqs")
+        dictionary = {}
+        for word in self.word_list:
+            if word in dictionary:
+                dictionary[word] += 1
+            else:
+                dictionary[word] = 1
+        letter_sorted = sorted(dictionary.items(), key=lambda entry: entry[0])   #sorts dictionary into alphabetized tuples
+        count_sorted = sorted(letter_sorted, key=lambda seq: seq[1], reverse=True) #sorts alphabetical tuples into count order
+        return count_sorted        
 
 
 class FreqPrinter:
     def __init__(self, freqs):
-        pass
+        self.freqs = freqs
 
     def print_freqs(self):
         """
@@ -67,8 +81,10 @@ class FreqPrinter:
        rights | 6    ******
         right | 6    ******
         """
-        raise NotImplementedError("FreqPrinter.print_freqs")
-
+        words = list(self.freqs)[0:10]
+        print()
+        for word in words:
+            print(word[0].rjust(15) + "  |  " + str(word[1]).ljust(3) + " " + (word[1] * "*"))
 
 if __name__ == "__main__":
     import argparse
